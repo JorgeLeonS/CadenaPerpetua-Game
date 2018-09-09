5c985baef3756bec0891aa05e28deebfb28f4e6b
Alf-Prado <alfpradoc@gmail.com>
2018-09-08 19:56:40 -0500

    Movimiento

--- ProyectoFinal/Assets/Scripts/Player.cs
+++ ProyectoFinal/Assets/Scripts/Player.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public int playerHealth = 5;
-    public Vector2 speed = new Vector2(5, 0);
+    public int speed = 10;
+    Vector2 forceSpeed = new Vector2(0, 5);
     private Rigidbody2D rigidbodyComponent;
-    Vector2 movementX = new Vector2(100, 0);
-    Vector2 movementXN = new Vector2(-100, 0);
     
 
 
@@ -19,30 +18,32 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+         
+        float inputX = Input.GetAxis("Horizontal") * speed;
+        float inputY = Input.GetAxis("Vertical") * speed;
+        inputX *= Time.deltaTime;
+        inputY *= Time.deltaTime;
+
+
+        transform.Translate(0, inputY, 0);
+        transform.Translate(inputX, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rigidbodyComponent.transform.Translate(1, 0, 0);
+            rigidbodyComponent.AddForce(forceSpeed);
         }
+        
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            rigidbodyComponent.transform.Translate(-1, 0, 0);
-        }
-
-        /*float inputX = Input.GetAxis("Horizontal");
-
-        movement = new Vector2(speed.x + inputX, 0);*/
-
-        bool shoot = Input.GetKeyDown(KeyCode.Space);
+       bool shoot = Input.GetKeyDown(KeyCode.Space);
   
         if (shoot)
         {
             Weapons weapon = GetComponentInChildren<Weapons>();
             if (weapon != null)
             {
-                // false because the player is not an enemy
                 weapon.Attack(false);
             }
+
         }
     }
 
