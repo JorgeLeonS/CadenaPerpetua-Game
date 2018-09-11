134d7297efddfd8da8a7760336bdda8354512bd7
Alf-Prado <alfpradoc@gmail.com>
2018-09-11 15:10:39 -0500

    Eliminación de redundancias enmúltiples códigos

--- ProyectoFinal/Assets/Scripts/PlayerShoot.cs
+++ ProyectoFinal/Assets/Scripts/PlayerShoot.cs
@@ -5,25 +5,13 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
     public int damage = 1;
 
-    public Vector2 speed = new Vector2(5, 0);
-    private Vector2 movement;
-    private Rigidbody2D rigidbodyComponent;
-
-    // Use this for initialization
-    void Start () {
-        Destroy(gameObject, 10);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        movement = new Vector2(speed.x, 0);
-    }
-
-    void FixedUpdate()
+    void Start()
     {
-        if (rigidbodyComponent == null)
-            rigidbodyComponent = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 2);
+    }
 
-        rigidbodyComponent.velocity = movement;
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector3(0.1f, 0, 0));
     }
 }
