a1f87ea552ab23fdce3f4b8d4b4f9d0c35d95e4b
Jorge <jorgemachu123@gmail.com>
2018-11-28 18:51:17 -0600

    Excena boss
    Se agregó excena de boss y las animaciones para golpe con cuchillo y macana

---
+++ ProyectoFinal/Assets/Scripts/MenuScripts/EnterBossLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterBossLevel : MonoBehaviour {
+
+    public void LoadBossLevel(string BossLevel)
+    {
+        SceneManager.LoadScene("BossLevel");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
