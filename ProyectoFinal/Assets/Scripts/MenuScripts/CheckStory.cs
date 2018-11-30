b3d98aa4140f930e7c6640fb8d5ab425cfbe9561
Jorge <jorgemachu123@gmail.com>
2018-11-30 10:16:06 -0600

    Menú Historia
    Se añadió el menú para ver la historia

---
+++ ProyectoFinal/Assets/Scripts/MenuScripts/CheckStory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckStory : MonoBehaviour {
+
+    public void SeeStory(string Story)
+    {
+        SceneManager.LoadScene("Story");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
